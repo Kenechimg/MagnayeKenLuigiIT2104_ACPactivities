7d393efed0a7c1f82e83103a25834c47557361bc
Kenechimg <magnayekenluigi@gmail.com>
2024-08-21 11:52:03 +0800

    001 | Exercise Set 1

---
+++ Magnaye, Ken Luigi M/Exercise Set 01/escseq.py
@@ -0,0 +1,5 @@
+for l in range(1, 6):
+    if l % 2 == 0:
+        print("\t" + str (l*7))
+    else:
+        print(l*7)
---
+++ Magnaye, Ken Luigi M/Exercise Set 01/tempconvert.py
@@ -0,0 +1,3 @@
+C= int(input("Enter a Celsius value:"))
+F= round(C*9/5 + 32, 2)
+print(f"{C: .2f} Celsius is{F: .2f} Farenheit")
