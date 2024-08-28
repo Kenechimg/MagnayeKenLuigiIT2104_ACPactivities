41760d50b411af053d48f65f23205a477961478f
Kenechimg <magnayekenluigi@gmail.com>
2024-08-28 14:42:17 +1000

    Add files via upload

---
+++ Exercise Set 02/bitwise1.py
@@ -0,0 +1,11 @@
+bit1 = 12 | 25
+bit2 = 21 & 27
+bit3 = 8 ^ 7
+bit4 = 13 & 7
+bit5 = 13 | 7
+
+print(f"a. 12 | 25 = {bit1}")
+print(f"b. 21 & 27 = {bit2}")
+print(f"c. 8 ^ 7 = {bit3}")
+print(f"d. 13 & 7 = {bit4}")
+print(f"e. 13 | 7 = {bit5}")
---
+++ Exercise Set 02/typecasting.py
@@ -0,0 +1,5 @@
+intnum = int(input("Enter an integer: "))
+floatnum = float(input("Enter a float: "))
+
+difference = intnum - floatnum
+print(f"Difference: { difference: .2f}")
